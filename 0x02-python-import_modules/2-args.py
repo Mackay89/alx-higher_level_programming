23acd853900bf50a68cfc76b570bee69a46c91e7
Mackay89 <manotomackay@gmail.com>
2024-01-04 15:17:42 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
         print("{} argument:".format(c))
     else:
         print("{} arguments:".format(c))
+
     for i in range(c):
         print("{}: {}".format(i + 1, argv[i +1]))
