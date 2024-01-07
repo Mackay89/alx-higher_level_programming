6c819375433d8bac872a2c3baccb350a8ea2bd20
Mackay89 <manotomackay@gmail.com>
2024-01-07 05:41:46 -0800

    11-delete_at.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 def multiple_returns(sentence):
     if not sentence:
         sentence = None
-        if sentence:
-            len_sent = len(sentence)
-        else:
-            len_sent = 0
-        return (len_sent, sentence if not sentence else sentence[:1])
+    if sentence:
+        len_sent = len(sentence)
+    else:
+        len_sent = 0
+    return (len_sent, sentence if not sentence else sentence[:1])
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,24 +1,24 @@
-#ifndef LISTS_H_
-#define LISTS_H_
-
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
- * structlistint_t - singly link list
+ * struct listint_s - singly linked list
  * @n: integer
- * @next: The points to the next node
- * Description: singly linked list node structurew
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for project
  */
-typedef struct listint_t
+typedef struct listint_s
 {
-	int n;
-	struct listint_t *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
-void free_listint(listint_t *head);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-int is_palindrome(listint_t **head);
 size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
-#endif /* LISTS_H_ */
+int is_palindrome(listint_t **head);
+
+#endif /* LISTS_H */
