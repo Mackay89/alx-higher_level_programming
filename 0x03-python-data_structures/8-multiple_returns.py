46c4f6f8368d7c8f23f8f983afb012b00af040b3
Mackay89 <manotomackay@gmail.com>
2024-01-07 05:04:17 -0800

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        sentence = None
+        if sentence:
+            len_sent = len(sentence)
+        else:
+            len_sent = 0
+        return (len_sent, sentence if not sentence else sentence[:1])
