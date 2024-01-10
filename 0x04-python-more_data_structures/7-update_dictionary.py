ab9d036370d15ba4902143488b8bbc689f5fe9d8
Mackay89 <manotomackay@gmail.com>
2024-01-10 13:07:26 -0800

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if item not in a_dictionary:
+        a_dictionary{item] = value
+    else:
+        for item in a_dictionary:
+            if item == item:
+                a_dictionary[item] = value
+    return a_dictionary
