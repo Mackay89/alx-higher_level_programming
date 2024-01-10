2fb99ebfe2cafcc0ca7299fc0eb69165808b56af
Mackay89 <manotomackay@gmail.com>
2024-01-10 11:40:42 -0800

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     common = set()
-    for elements in (set_1, set_2):
-        common.add(elements)
+    for elements in set_1:
+        if elements in set_2:
+            common.add(elements)
     return common
