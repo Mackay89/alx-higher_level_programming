2120e00afc457cfc21b5f587c6cd25a9248ee2dc
Mackay89 <manotomackay@gmail.com>
2024-01-10 13:43:15 -0800

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dic = a_dictionary.copy()
-    new_dic[key] = value
-    return new_dic
+    new_diconary = a_dictionary.copy()
+    new_dictionary[key] = value
+    return new_dictionary
 
