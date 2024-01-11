ac41120cfde4111cf1300c43bdd789b02bb24ecb
Mackay89 <manotomackay@gmail.com>
2024-01-10 16:20:00 -0800

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,12 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    numer = 0
+    num = 0
     den = 0
     for i in range(len(my_list):
-        numer += my_list[i][0] * my_list[1][i]
+        num += my_list[i][0] * my_list[1][i]
         den += my_list[1][i]
-    return (num / den)
+        avarage = num / den
+   return avarage
+   else:
+   return 0
