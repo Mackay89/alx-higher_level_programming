38fa60a454afa101640ab57ef0f4c69f887e5ff2
Mackay89 <manotomackay@gmail.com>
2024-01-10 16:36:37 -0800

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,6 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    avarage = sum(avar * weight for avar, weight in my_list)
+    avar = sum(avar * weight for avar, weight in my_list)
     weight = sum(weight for _, weight in my_list)
     return avar / weight
