1f0eddb3a8fbe7748bd7c83a4089da9b8a68e6e7
Mackay89 <manotomackay@gmail.com>
2024-01-22 13:32:07 -0800

    print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,12 +7,13 @@ Args:
     my_list (list: The list to print element from.
     x (int): The number of the elements of my_list to print.
 
-    Return: The number of elements printed.
+    Return:
+    The number of elements printed.
     """
     sum  = 0
     for i in range(x):
         try:
-            print("{}".format(my_list=[i]), end="")
+            print("{}".format(my_list[i]), end="")
             sum = sum + 1
         except IndexError:
             continue
