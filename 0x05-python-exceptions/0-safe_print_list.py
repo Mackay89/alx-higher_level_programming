d0bd7b0ddc68bc1274cc1a8e9e58d86b8448100f
Mackay89 <manotomackay@gmail.com>
2024-01-22 12:57:30 -0800

    print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0);
-    if my_list is None:
-        my_list = []
-
-    count = 0
-
+    sum  = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
-            count = count + 1
+            print("{}".format(my_list=[i]), end="")
+            sum = sum + 1
         except IndexError:
             continue
 
     print("")
-    return count
+    return (sum)
