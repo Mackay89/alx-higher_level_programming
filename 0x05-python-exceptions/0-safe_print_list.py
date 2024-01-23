d11f0e0d9f59b7bffae9d7e4fb79df120ff5eb5e
Mackay89 <manotomackay@gmail.com>
2024-01-23 08:04:44 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=None, x=0):
     sum_count  = 0
 
     if my_list is None:
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
+def safe_print_list_integers(my_list=None, x=0):
     count = 0
 
     if my_list is None:
         my_list = []
 
-    for c in range(0, x):
+    for i in range(0, x):
         try:
-            print("{:d}".format(my_list[c]), end='')
+            print("{:d}".format(my_list[i]), end='')
             count += 1
     except (TypeError, ValueError):
-        continue
+        pass
 
     print("")
-    return (count)
+    return count
