5ade5d454affa7a36084a84ee5be7991eecb2c95
Mackay89 <manotomackay@gmail.com>
2024-01-24 13:27:02 -0800

    complite

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,11 +1,41 @@
 #!/usr/bin/python3
-"""A module Square"""
+"""
+A module Square.
+
+This module contains the Square class that represent a square
+with a specified size and position.
+"""
 
 
 class Square:
-    """Represent a square."""
+    """
+    Represent a square.
+
+
+    Attributes:
+        __size (int): The size of the square.
+        __position (tuple): The position of the square.
+
+    Methods:
+        __init__(self, size=0, position=(0, 0)): Initializes a new Square instance.
+        size (property): Getter and setter for size.
+        position (property): Getter and setter for position.
+        area(self): Calculate the area of the square.
+    """
+
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new Square instance."""
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+            position (tuple): The position of the square.
+        
+        Raises:
+            TypeError: If size is not an integer or if position is not a tuple
+            of 2 positive integer.
+        """
+
         self.size = size
         self.position = position
 
@@ -19,7 +49,9 @@ class Square:
         """setter method for size."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif:
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
             self.__size = value
 
     @property
