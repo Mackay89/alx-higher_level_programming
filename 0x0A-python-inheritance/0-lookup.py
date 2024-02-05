c053983ea3f8fb1bf227488258db91c70f7e2ca0
Mackay89 <manotomackay@gmail.com>
2024-02-05 08:51:41 -0800

    text

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Creat a  function that returns the list of available attributes and methods of an object.
+"""
+
+
+def lookup(obj):
+    """
+    Represent a return list of an object's that available attributes.
+    """
+
+    return (dir(obj))
