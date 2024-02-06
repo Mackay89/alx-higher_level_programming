b63a0098aea1be15dd446925c3498002a43227c3
Mackay89 <manotomackay@gmail.com>
2024-02-06 15:05:45 -0800

    text

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,7 +4,7 @@ Module of a script that reads from  standard input and commputes metrics.
 
 After every ten lines or the input of a keyboard interruption (CTRL + C), prints te following statistics:
     - Total file size up to that point.
-    -  Count of read status codes up to that point.
+    - Count of read status codes up to that point.
 """
 
 
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 
     size = 0
     status_codes = {}
-    valid_codes = [200, 301, 400, 401, 403, 404, 405 and 500]
+    valid_codes = [200, 301, 400, 401, 403, 404, 405, 500]
     count = 0
 
 
@@ -46,16 +46,15 @@ if __name__ == "__main__":
                 pass
 
             try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
+                if int(line[-2]) in valid_codes:
+                    if status_codes.get(line[-2]) is None:
+                            status_codes[line[-2]] = 1
                     else:
                         status_codes[line[-2]] += 1
-            except IndexError:
+            except (IndexError, ValueError):
                 pass
 
-        print_stats(size, status_codes0
-
+        print_stats(size, status_codes)
     except KeyboardInterrupt:
-    print_stats(size, status_codes)
-    raise
+            print_stats(size, status_codes)
+            raise
