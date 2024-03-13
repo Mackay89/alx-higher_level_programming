218601508dbb903ce2625cabe516372ce1685862
Mackay89 <manotomackay@gmail.com>
2024-03-13 14:00:05 -0700

    task

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- task: Script to create table force_name on MySqL server.
 -- creates a table in database
-GREATE TABLE IF NOT EXISTSS force_name (id INT, name VARCHAR(256) NOT NULL);
+GREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
