0dd4b0461cfdfd1bb059df2bf4d790a5c0bdfb9b
Mackay89 <manotomackay@gmail.com>
2024-03-14 11:08:50 -0700

    task

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,3 +1,8 @@
 -- task: Script to list all genres not linked to the show Dexter from the hbtn_0d_tvshows database
 -- lists all rows of a table linked to another table
-SELECT title, name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY title ASC, name ASC;
+SELECT title, name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title != 'Dexter' or tv_shows.title IS NULL
+ORDER BY title ASC, name ASC;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,9 @@
 -- task: Script to list all shows and their linked genres from the hbtn_0d_tvshows database
 -- lists all rows of a database corresponding to a column value
-SELECT title FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name = 'Comedy' GROUP BY title ORDER BY title ASC;
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+GROUP BY tv_shows.title
+ORDER BY tv_shows.title ASC;
