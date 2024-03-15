72818b9f8db136c357c4d464f1e2e87532d3b0dc
Mackay89 <manotomackay@gmail.com>
2024-03-14 19:58:57 -0700

    task

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Creates and fills a table second_table with attributes id, name and score.
-CREATE TABLE IF NOT EXISTS 'second_table' ('id' INT, 'name' VARCHAR(256), 'score' INT);
-INSERT INTO 'secod_table' ('id', 'name', 'score') VALUES (1, "John", 10);
-INSERT INTO 'secod_table' ('id', 'name', 'score') VALUES (1, "Alex", 3);
-INSERT INTO 'secod_table' ('id', 'name', 'score') VALUES (1, "Bob", 14);
-INSERT INTO 'secod_table' ('id', 'name', 'score') VALUES (1, "George", 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO secod_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO secod_table (id, name, score) VALUES (1, "Alex", 3);
+INSERT INTO secod_table (id, name, score) VALUES (1, "Bob", 14);
+INSERT INTO secod_table (id, name, score) VALUES (1, "George", 8);
