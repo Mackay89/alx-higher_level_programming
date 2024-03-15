220f9d1db4d6a8ad3fe42948f6852cb1b1d2d51c
Mackay89 <manotomackay@gmail.com>
2024-03-14 19:43:26 -0700

    task

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- task: Creates a table called first_table with values id and name.
-CREATE TABLE IF NOT EXISTS 'first_table' (
-	'id' INT,
-       	'name' VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+       	name VARCHAR(256)
 );
