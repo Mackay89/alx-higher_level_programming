28f445265ebac5839186be5b50cf0276db0b38d6
Mackay89 <manotomackay@gmail.com>
2024-03-14 19:48:22 -0700

    task

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- task: Inserts a new row into the table  first_table.
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, "Best School");
+INSERT INTO first_table (id, name) VALUES (89, Best School);
