ba09f560bf7dd1ae160274871ae6c4dc0044966a
Mackay89 <manotomackay@gmail.com>
2024-03-14 18:49:32 -0700

    task

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,6 +2,6 @@
 -- lists all rows of a table linked to another table
 SELECT name
 FROM tv_genres
-WHERE name NOT IN (SELECT name FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genres_id LEFT JOIN  tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = 'Dexter')
+WHERE name NOT IN (SELECT name FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id LEFT JOIN  tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = 'Dexter')
 GROUP BY name
 ORDER BY name ASC;
