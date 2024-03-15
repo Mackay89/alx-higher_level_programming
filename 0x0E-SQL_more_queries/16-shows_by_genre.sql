1f80d5ae86eb489ab2331eb0dbffcfc28bf6f243
Mackay89 <manotomackay@gmail.com>
2024-03-14 18:20:15 -0700

    task

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,8 @@
 -- task: Script to list all shows and their linked genres from the hbtn_0d_tvshows database
 -- lists all rows of a database corresponding to a column value
-SELECT tv_shows.title
+SELECT title, name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
-GROUP BY tv_shows.title
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title ASC, name ASC
