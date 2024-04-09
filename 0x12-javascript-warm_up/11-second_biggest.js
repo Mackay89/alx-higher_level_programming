7019c3ebbf240a82674146eb350f20e933e76e13
Mackay89 <manotomackay@gmail.com>
2024-04-08 19:23:20 -0700

    task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const argumentsList  = process.argv.slice(2);
-const number = argumentsList.map(argv => parseInt(arg));
+const args = process.argv.slice(2);
 
-if (number.lenght < 2) {
+if (args.length < 2 || !args.every(argv => !isNaN(argv))) {
 	console.log(0);
 } else {
+	const numbers = args.map(Number);
 	const sortedNumbers = numbers.sort((a, b) => b - a);
 	console.log(sortedNumbers[1]);
 }
