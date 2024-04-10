cbf3a7f81f59c096f1da8e72bc0f4b7d0d0dbcbb
Mackay89 <manotomackay@gmail.com>
2024-04-10 11:25:18 -0700

    task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-if (arg === undefined) {
+const argv = process.argv[2];
+if (argv === undefined) {
 	console.log('No argument');
 } else {
-	console.log(arg);
+ console.log(argv);
 }
