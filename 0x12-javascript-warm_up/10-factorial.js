1396eb9891231e91336d08fbe46a3b9401ef0894
Mackay89 <manotomackay@gmail.com>
2024-04-10 12:40:00 -0700

    task

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
+
 function factorial (n) {
-	if (n < 0) {
-		return (-1);
+	if (n === 1) {
+		return 1;
+	} else {
+		return n * factorial(n - 1);
 	}
-	if (n === 0 || isNaN(n)) {
-		return (1);
-	}
-	return (n * factorial(n - 1));
 }
 
-console.log(factorial(Number(process.argv[2])));
+if (isNaN(process.argv[2])) {
+	console.log(1);
+} else {
+	console.log(factorial(parseInt(process.argv[2])));
+}
