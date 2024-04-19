0bfaf579ae352c50a2ea566f6be88436466458b2
Mackay89 <manotomackay@gmail.com>
2024-04-19 12:02:55 -0700

    task

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -10,13 +10,16 @@ from sqlalchemy.orm  import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for state in session.query(State).order_by(State.id):
-        print(state.id, state.name, sep=": ")
-        for city in state.cities:
-            print("   ", end="")
-            print("\t{}: {}".format(city.id, city.name))
-    session.close()
+    try:
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        for state in session.query(State).order_by(State.id):
+            print(state.id, state.name, sep=": ")
+            for city in state.cities:
+                print("   ", end="")
+                print("\t{}: {}".format(city.id, city.name))
+        session.close()
+    except Exception as e:
+        print("An error occurred:", e)
