6ba12316013fd736707f1eaeb492469b443d2298
Mackay89 <manotomackay@gmail.com>
2024-04-19 14:45:53 -0700

    task

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -9,20 +9,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-            if len(sys.argv) != 5:
-                print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
-                sys.exit(1)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
 
-            try:
-                engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
-                Base.metadata.create_all(engine)
-                Session = sessionmaker(bind=engine)
-                session = Session()
-                for state in session.query(State).order_by(State.id):
-                    print(state.id, state.name, sep=": ")
-                    for city in state.cities:
-                        print("  {} : {}".format(city.id, city.name))
-                session.close()
-            except Exception as e:
-                print("An error occurred:", e)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
+    for city, state in session.query(City, State).join(State, State.id == City.state_id).order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, state.name))
+                
