1f38b8c1ebada4d06d1f64e39ddeb97746148365
Mackay89 <manotomackay@gmail.com>
2024-04-25 10:53:45 -0700

    task

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+#!/bin/bash
 #Script that takes in a URL, sends a request to that URL, and displays size of the body of the response.
 
 if [ -z "$1" ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
+    echo "Usage: $0 <URL>"
+    exit 1
 if
 
 curl -s "$1" | wc -c 
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/bin/bash
 #Script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 
 
