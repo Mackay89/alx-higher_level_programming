ccbfeb5b0b18fac3b5217e68c6899984d14638f5
Mackay89 <manotomackay@gmail.com>
2024-04-26 13:17:11 -0700

    task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+import urllib.request
+
+
+#Represent the URL
+url = "https://alx-intranet.hbtn.io/status"
+
+
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+except Exception as e:
+    print("An error occurred:", e)
