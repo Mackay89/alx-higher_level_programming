e195d4f490e68012784f7efd3297a9e2a3cf49fb
Mackay89 <manotomackay@gmail.com>
2024-04-26 13:31:38 -0700

    task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,15 +7,22 @@ Script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
 
-#Represent the URL
-url = "https://alx-intranet.hbtn.io/status"
+def fetch_status():
+    """
+    Fetch the status fromthe URL and prints response details
+    """
+    url = "https://alx-intranet.hbtn.io/status"
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
 
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read().decode('utf-8')
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-except Exception as e:
-    print("An error occurred:", e)
+            print("Body response:")
+            print("\t- type:", type(body))
+            print("\t- content:", body)
+    except Exception as e:
+        print("An error occurred:", e)
+
+
+if __name__ == "__main__":
+    fetch_status()
