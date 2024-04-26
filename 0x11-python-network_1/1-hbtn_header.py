e9a5f3713a51c4693958fbb174ca0d0d350f57f2
Mackay89 <manotomackay@gmail.com>
2024-04-26 15:58:51 -0700

    task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,27 +8,10 @@ import sys
 import urllib.request
 
 
-def x_request_id(url):
-    """
-    Sends a request to URL and displays the value of the  X-Request-id
-    variable found in the header of the response.
-    """
-    try:
-        with urllib.request.urlopen(url) as response:
-            x_request_id = response.headers.get("X-Request-Id")
-            if x_request_id:
-                print(x_request_id)
-            else:
-                print("No X-Request-Id found in the response headers.")
-    except Exception as e:
-        print("An error occurred:", e)
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <URL>".format(sys.argv[0]))
-        sys.exit(1)
-
-
     url = sys.argv[1]
-    x_request_id(url)
+
+
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.headers.get("X-Request-Id")
+        print(x_request_id)
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 Script that takes in a URL and email, sends a POST request to the passed URL
-with the email as parameter, and displays the body of the response(decoded
-in utf-8)
+with the email as parameter, and displays the body of the response
+(decoded in utf-8)
 """
 
 
