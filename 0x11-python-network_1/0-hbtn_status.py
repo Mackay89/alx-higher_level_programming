4a2d58bfc8c2e96f5b8f06e6a42ac3de2da5f6b7
Mackay89 <manotomackay@gmail.com>
2024-04-28 09:24:50 -0700

    task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,8 @@
 Script that fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
+
+
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
@@ -10,4 +12,4 @@ if __name__ == "__main__":
         print("body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf-8 content: {}".format(body.decode("utf-8")))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
