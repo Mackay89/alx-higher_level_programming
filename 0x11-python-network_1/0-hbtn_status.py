b60d44862cf68ca2870a04ac80050107b04494fc
Mackay89 <manotomackay@gmail.com>
2024-04-28 04:09:49 -0700

    task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,18 +6,11 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    try:
-        url = "https://alx-intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
         request = urllib.request.Request(url)
-
-
         with urllib.request.urlopen(request) as response:
             body = response.read().decode('utf-8')
-
-
             print("Body response:")
             print("\t- Type: {}".format(type(body)))
             print("\t- Content: {}".format(body))
             print("\t- UTF-8 content: {}".format(body))
-    except Exception as e:
-        print("An error occurred:", e)
