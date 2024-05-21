a3967372f96cc604139ce2afbb47f979d7362cc1
Mackay89 <manotomackay@gmail.com>
2024-05-21 15:45:20 -0700

    task

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
 
-
 const request = require('request');
-const util = require('util');
 
 
-const get = util.promisify(request.get);
-
-
-get(process.argv[2])
-  .then(response => {
-    console.log('Status code: ${response.status}');
-  })
-  .catch(error => {
-    console.error(error);
-  });
+request(process.argv[2], function (err, response) {
+    console.log('code:', response.statusCode);
+});
