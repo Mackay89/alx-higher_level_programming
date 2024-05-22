987cb3e3035355895a6de3d4df5d7ca929486894
Mackay89 <manotomackay@gmail.com>
2024-05-21 20:59:17 -0700

    task

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,52 @@
+#!/usr/bin/node
+
+
+const request = require('request');
+
+
+const movieId = process.argvp[2];
+const apiUrl = 'https://swapi.dev/api/films/${moviesId}/';
+
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+
+  if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode)
+    return;
+  }
+
+
+  const film = JSON.parse(body);
+  const charactersUrls = film,characters;
+
+
+  const	 getCharacterName = (Url) => {
+    return new Promise((resolve, reject) => }
+      request(url, function (error, response, body) {
+        if (error) {
+          reject(error);
+	} else {
+	  const character = JSON.parse(body);
+          resolve(character.name);
+	}
+      });
+});
+};
+
+
+Promise.all(charactersUrls.map(getCharacterName))
+  .then((characters) => {
+    characters,forEach((character) => {
+      console.log(character);
+    });
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+});
+
