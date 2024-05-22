6cf926398e17cb81c3031364d754f87a33f106bb
Mackay89 <manotomackay@gmail.com>
2024-05-22 12:41:00 -0700

    task

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,48 +4,53 @@
 const request = require('request');
 
 
-const movieId = process.argv[2];
-const apiUrl = 'https://swapi.dev/api/films/${movieId}/';
-
-
-request(apiUrl, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-
-  if (response.statusCode !== 200) {
-    console.error('Status:', response.statusCode)
-    return;
-  }
-
-
-  const film = JSON.parse(body);
-  const charactersUrls = film.characters;
-
-
-  const	 getCharacterName = (url) => {
-    return new Promise((resolve, reject) => {
-      request(url, function (error, response, body) {
-        if (error) {
-          reject(error);
+function getDataFrom(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, resp, body) => {
+       if (error) {
+	 reject(error);
 	} else {
-	  const character = JSON.parse(body);
-          resolve(character.name);
+	  resolve(body);
 	}
-      });
     });
-  };
+  });
+}
 
 
-  Promise.all(charactersUrls.map(getCharacterName))
-    .then((characters) => {
-      characters.forEach((character) => {
-        console.log(character);
-      });
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-});
+function errorHandler(error) {
+  console.error(error);
+}
+
+
+async function printMovieCharacters(movieId) {
+  try {
+    const movieUri = 'https://swapi.dev/api/films/${movieId}';
+    const movieData = await getDataFrom(movieUri);
+    console.log("Movie Data:", movieData);
+    const movie = JSON.parse(movieData);
+    console.log("Movie:", movie);
+    const characters = movie.characters;
+    console.log("Character:", characters);
+
+
+    const promise = characters.map(characterUrl => getDataFrom(characterUrl));
+    const characterResponses = await Promise.all(promises);
+
+
+    for (const characterResponse of characterResponses) {
+      const character = JSON.parse(characterResponse);
+      console.log(character.name);
+    }
+  } catch (error) {
+    errorHandler(error);
+  }
+}
+
+
+  const movieId = process.argv[2];
+  if (!movieId) {
+    console.error("Please provide a movie ID.");
+  } else {
+    printMovieCharacters(movieId)
+  }
+
